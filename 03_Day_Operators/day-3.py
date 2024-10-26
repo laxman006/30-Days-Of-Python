d988d11dabe3940e47b7c0313dd55f91aae2969a
Sai Laxman Kadari <165615041+laxman006@users.noreply.github.com>
2024-10-26 23:50:07 +0530

    Update day-3.py

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -122,3 +122,17 @@ print(not True)      # False - Negation, the not operator turns true to false
 print(not False)     # True
 print(not not True)  # True
 print(not not False) # False
+
+
+///try///
+
+print('Addition: ', 1 + 2)
+print('Subtraction: ', 2 - 1)
+print('Multiplication: ', 2 * 3)
+print ('Division: ', 4 / 2)                         # Division in python gives floating number
+print('Division: ', 6 / 2)
+print('Division: ', 7 / 2)
+print('Division without the remainder: ', 7 // 2)   # gives without the floating number or without the remaining
+print('Modulus: ', 3 % 2)                           # Gives the remainder
+print ('Division without the remainder: ', 7 // 3)
+print('Exponential: ', 3 ** 2)                     # 
